44beb03d5a08b7ba9ab86e38c46b3fb808057626
Towfique Kabir <towfique.pranto@gmail.com>
2026-03-12 03:20:34 +0600

    Module 5 Lesson 7

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,12 +3,30 @@ import Image from "./Image";
 import WorkSample from "./WorkSample";
 
 function App() {
+  const workData = [
+    {
+      year: "2018-2022",
+      exp: "Bsc in CSE",
+      inst: "EWU",
+    },
+    {
+      year: "2023-2026",
+      exp: "Bsc in CSE",
+      inst: "UIU",
+    },
+    {
+      year: "2026-current",
+      exp: "Full Stack Developer",
+      inst: "Future tech",
+    },
+  ];
   return (
     <div>
+      {workData.map((work) => (
+        <WorkSample work={work} />
+      ))}
       <Image />
       <Bio />
-      <WorkSample year={"2018-2022"} exp={"Bsc in CSE"} inst={"UIU"} />
-      <WorkSample year={"2023-2026"} exp={"Bsc in CSE"} inst={"UIU"} />
     </div>
   );
 }
--- react/src/WorkSample.jsx
+++ react/src/WorkSample.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-export default function WorkSample({ year, exp, inst }) {
+export default function WorkSample({ work }) {
+  const myStyle = {
+    border: "1px solid black",
+    margin: "10px",
+    padding: "10px",
+    backgroundColor: "#f0f0f0",
+  };
   return (
-    <div>
-      <span>{year}</span>
-      <h2>{exp}</h2>
-      <p>{inst}</p>
+    <div style={myStyle}>
+      <span>{work.year}</span>
+      <h2>{work.exp}</h2>
+      <p>{work.inst}</p>
     </div>
   );
 }
