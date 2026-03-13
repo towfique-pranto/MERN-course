0a69c8f7d63bee86abd21ff6ea89b67b812c6f6b
Towfique Kabir <towfique.pranto@gmail.com>
2026-03-14 02:40:29 +0600

    Module 5 Lesson 17: React Form Part 2

--- react/src/Form.jsx
+++ react/src/Form.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Form() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    gender: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
   const submitForm = (e) => {
     e.preventDefault();
-    alert("Form Submitted");
+    console.log("Form Submitted", formData);
   };
   return (
-    <div className="max-w-md mx-auto">
+    <div className="max-w-md mx-auto border-2 p-4">
       <form onSubmit={submitForm} className="space-y-4">
         <div>
           <label
@@ -16,6 +26,7 @@ export default function Form() {
             Name
           </label>
           <input
+            onChange={handleChange}
             type="text"
             id="name"
             name="name"
@@ -30,6 +41,7 @@ export default function Form() {
             Email
           </label>
           <input
+            onChange={handleChange}
             type="email"
             id="email"
             name="email"
@@ -44,11 +56,32 @@ export default function Form() {
             Password
           </label>
           <input
+            onChange={handleChange}
             type="password"
             id="password"
             name="password"
             className="my-1 p-2 input input-bordered w-24 md:w-auto"
           ></input>
+        </div>
+        <div>
+          <label
+            htmlFor="gender"
+            className="block text-sm font-medium text-white"
+          >
+            Gender
+          </label>
+          <select
+            onChange={handleChange}
+            id="gender"
+            name="gender"
+            defaultValue="Pick a gender"
+            className="my-1 select"
+          >
+            <option disabled={true}>Pick a gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="others">Others</option>
+          </select>
         </div>
         <div>
           <button type="submit" className="btn btn-success">
