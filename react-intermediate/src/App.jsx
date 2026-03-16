edbc2a31f5d77f9a5316032f05d367256865d934
Towfique Kabir <towfique.pranto@gmail.com>
2026-03-17 03:41:19 +0600

    Module 6 Lesson 3: React Router DOM 6 Installation and Setup

--- react-intermediate/src/App.jsx
+++ react-intermediate/src/App.jsx
@@ -1,35 +1,10 @@
-import { useEffect, useState } from "react";
 import BasicExample from "./BasicExample";
 import { Routes, Route } from "react-router";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    fetch("https://dummyjson.com/users")
-      .then((res) => res.json())
-      .then((data) => setUsers(data.users));
-  }, []);
-
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <div className="d-flex gap-3 flex-wrap justify-content-center">
-            {users.map((user) => {
-              return (
-                <BasicExample
-                  key={user.id}
-                  firstName={user.firstName}
-                  lastName={user.lastName}
-                  image={user.image}
-                  university={user.university}
-                />
-              );
-            })}
-          </div>
-        }
-      />
+      <Route path="/" element={<BasicExample />} />
       <Route path="/user" element={<h1>User Details</h1>} />
     </Routes>
   );
--- react-intermediate/src/BasicExample.jsx
+++ react-intermediate/src/BasicExample.jsx
@@ -1,18 +1,33 @@
+import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-function BasicExample({ firstName, lastName, image, university }) {
+function BasicExample() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data.users));
+  }, []);
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={image} />
-      <Card.Body>
-        <Card.Title>
-          {firstName} {lastName}
-        </Card.Title>
-        <Card.Text>{university}</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <div className="d-flex gap-3 flex-wrap justify-content-center">
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={user.image} />
+              <Card.Body>
+                <Card.Title>
+                  {user.firstName} {user.lastName}
+                </Card.Title>
+                <Card.Text>{user.university}</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
