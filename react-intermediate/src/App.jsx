4ae21f1fc2d8e560c8a622a1daf3e324b60a4475
Towfique Kabir <towfique.pranto@gmail.com>
2026-03-18 23:36:43 +0600

    Add User component and update routing in App component

--- react-intermediate/src/App.jsx
+++ react-intermediate/src/App.jsx
@@ -2,13 +2,14 @@ import BasicExample from "./BasicExample";
 import { Routes, Route } from "react-router";
 import ErrorPage from "./ErrorPage";
 import Layout from "./Layout";
+import User from "./User";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<BasicExample />} />
-        <Route path="/user" element={<h1>User Details</h1>} />
+        <Route path="/user" element={<User />} />
       </Route>
       <Route path="*" element={<ErrorPage />} />
     </Routes>
---
+++ react-intermediate/src/User.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function User() {
+  return <div>I am a User and my ID is 123</div>;
+}
