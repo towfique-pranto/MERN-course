a2e9f5a40766c321dce265cb98f33f3a09cf7d85
Towfique Kabir <towfique.pranto@gmail.com>
2026-03-23 01:19:34 +0600

    Module 7 Lesson 2: Register a User with Firebase

--- firebase/src/Register.jsx
+++ firebase/src/Register.jsx
@@ -1,18 +1,56 @@
+import { useState } from "react";
+import { createUser } from "./firebase/firebase.config";
+
 function Register() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  // function to handle input changes for both email and password
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+    console.log("Current user state:", user);
+  };
+
+  // function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await createUser(user.email, user.password);
+    console.log("Submitted:", user);
+  };
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
         <legend className="fieldset-legend">Signup</legend>
 
         <label className="label">Email</label>
-        <input type="email" className="input" placeholder="Email" />
+        <input
+          name="email"
+          type="email"
+          className="input"
+          value={user.email}
+          onChange={handleChange}
+        />
 
         <label className="label">Password</label>
-        <input type="password" className="input" placeholder="Password" />
+        <input
+          name="password"
+          type="password"
+          className="input"
+          value={user.password}
+          onChange={handleChange}
+        />
 
-        <button className="btn btn-neutral mt-4">Signup</button>
+        <button type="submit" className="btn btn-neutral mt-4">
+          Signup
+        </button>
       </fieldset>
-    </div>
+    </form>
   );
 }
 
